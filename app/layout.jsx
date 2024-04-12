5d2b6c9f7bc34cb085439677933f41a6899fd912
yzy-from-X1 <zongyuan1998@gmail.com>
2024-04-11 18:48:00 -0700

    seo4

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/react";
 
 // Define your metadata more extensively
-const metadata = {
+export const metadata = {
   title: "Interactive 454 Retail Calendar",
   description:
     "Interact with this interactive 454 Calendar (Retail Industry Standard from NRF) to pick date and calculate timeframe effortlessly and mistake free. Restated calendar included to deal with 53-week years.",
@@ -19,27 +19,14 @@ const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={metadata.description} />
-        <meta name="keywords" content={metadata.keywords} />
-        <meta name="author" content={metadata.author} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <title>{metadata.title}</title>
-      </Head>
+    <html lang="en">
       <body className="flex flex-col">
         <NavMenu />
+        {/* Adjust the mt-x class as needed to ensure enough space for the NavMenu */}
         <main className="flex flex-col mt-20">{children}</main>
         <Analytics />
         <Toaster position="bottom-right" reverseOrder={false} />
       </body>
-    </>
+    </html>
   );
 }
